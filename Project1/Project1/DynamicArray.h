53520ec0c5993441f6ea1e1cdec65d3ec7e508e6
Jake-Ailey <36465180+Jake-Ailey@users.noreply.github.com>
2018-04-06 16:27:43 +1000

    Finished the initial tasks, completing all basic functions necessary for a functioning dynamic array. Added in Pipe Comments throughout the code, deleted unnecessary chunks of code, and generally neatened the code up for the sake of my eyes. Also added in some "challenge" functions to complete in the future, that would a nice little way to fully finalise this Dynamic Array class.

--- Project1/Project1/DynamicArray.h
+++ Project1/Project1/DynamicArray.h
@@ -5,6 +5,12 @@ template<typename T>
 class DynamicArray
 {
 public:
+	//--------------------------------------------------------------------------------------------------|
+	//										CONSTRUCTOR													|
+	//	When a new Dynamic Array is created, the user will need to input the array type, and the initial|
+	//	size of the array. Example: DynamicArray<int> myArray(10);										|
+	//--------------------------------------------------------------------------------------------------|
+
 	DynamicArray(int size)
 	{
 		data = new T[size];
@@ -18,70 +24,43 @@ public:
 		delete[] data;
 	}
 
-	void ResizeArray()
-	{
+	//--------------------------------------------------------------------------------------------------|
+	//										RESIZE ARRAY												|
+	//	A function to resize the array by twice it's original size. Primarily called inside other		|
+	//	functions.																						|
+	//--------------------------------------------------------------------------------------------------|
+															
+	void ResizeArray()										
+	{														
 		T* newData = new T[(m_nArraySize * 2)];					//Creates a new array with twice the size as the original one
 		memcpy(newData, data, m_nArraySize * sizeof(T));		//'memcpy' copies all data from the original array into the new one
 		delete data;											//Deletes the old array
 		data = newData;											//Copies the new, bigger array into the original data variable
 		m_nArraySize *= 2;										//Doubles the ArraySize variable to match the new array size.
-	}
-
-	//Function to add to the array and resize it if it is already full
+	}														
+															
+	//--------------------------------------------------------------------------------------------------|
+	//										ADD TO ARRAY END											|
+	//	Function to add elements to the end of the array and resize it if already full					|
+	//--------------------------------------------------------------------------------------------------|
 	void AddToArrayEnd(T newdata)
-	{
-		if (m_nUsedElements == m_nArraySize)						//Checks to see if the array is already full
+	{	//Function to add to the array and resize it if it is already full
+
+		if (m_nUsedElements == m_nArraySize)						
 		{
 			ResizeArray();
 		}
 
-		data[m_nUsedElements] = newdata;							//Adds the new data into the array, then increments the Used Elements variable
-		m_nUsedElements += 1;										//Increments the UseElements variable so we know where we're up to		
+		data[m_nUsedElements] = newdata;							
+		m_nUsedElements += 1;										
 	}
-
-	//void ResizeArrayManual(char operation, int size)				//Function to resize the array
-	//{
-	//	if (operation == '*')									//Switch statement to find which operator the user specified
-	//	{														//Multiplies the array size by the given amount
-	//		T* newData = new T[m_nArraySize * size];
-	//		memcpy(newData, data, m_nArraySize * sizeof(T));
-	//		delete data;
-	//		data = newData;
-	//		m_nArraySize *= size;
-	//	}
-
-	//	else if (operation == '/')											//Divides the array size by the given amount
-	//	{
-	//		T* newData = new T[m_nArraySize / size];
-	//		memcpy(newData, data, m_nArraySize / sizeof(T));
-	//		delete data;
-	//		data = newData;
-	//		m_nArraySize /= size;
-	//	}
-	//	else if (operation == '+')											//Adds onto the array size by the given amount
-	//	{
-	//		T* newData = new T[m_nArraySize + size];
-	//		memcpy(newData, data, m_nArraySize + sizeof(T));
-	//		delete data;
-	//		data = newData;
-	//		m_nArraySize += size;
-	//	}
-	//	else if (operation == '-')											//Subtracts from the array size by the given amount
-	//	{
-	//		T* newData = new T[m_nArraySize - size];
-	//		memcpy(newData, data, m_nArraySize - sizeof(T));
-	//		delete data;
-	//		data = newData;
-	//		m_nArraySize -= size;
-	//	}
-	//	else
-	//	{
-	//		assert("Incorrect or no operator in use");
-	//	}
-	//}
-		
-	void PrintArray()											//Function to print out a visual representation of the array in it's entirety
-																//Purely for testing purposes
+			
+	//--------------------------------------------------------------------------------------------------|
+	//										PRINT ARRAY													|
+	//	Prints out a visual representation of what your array looks like. Primarily for testing purposes|
+	//--------------------------------------------------------------------------------------------------|
+	void PrintArray()										
+															
 	{
 		std::cout << "{";
 		for (int i = 1; i < m_nUsedElements + 1; i++)
@@ -91,22 +70,36 @@ public:
 		std::cout << "\b\b}" << std::endl;
 	}
 
-	void PrintArraySingle(int index)							//Function to print out a single piece of data, indexed by the user
-	{															//May not ever be needed, but can't hurt to include anyway
+	//--------------------------------------------------------------------------------------------------|
+	//									PRINT ARRAY SINGLE												|
+	//	Prints out a single piece of data from the array, at a point specified by the user				|
+	//--------------------------------------------------------------------------------------------------|
+	void PrintArraySingle(int index)							
+	{															
 		std::cout << data[index - 1] << std::endl;
 	}
 	
-	void RemoveFromEnd(int number)								//Number of entities to remove from the end of the array
+	//--------------------------------------------------------------------------------------------------|
+	//									REMOVE FROM END													|
+	//	A function to remove a number of entities from the end of the array, specified in the parameter |
+	//--------------------------------------------------------------------------------------------------|
+	void RemoveFromEnd(int number)							
 	{
 		for (int i = 0; i < number; i++)
 		{
-			m_nUsedElements -= 1;								//Lowers UsedElements, so that the next time it is written to, it will simply be
-		}														//overwrittten with the new data
+			m_nUsedElements -= 1;							
+		}													
 	}
 
-	void InsertInMiddle(T newData, int index)					//Inserts the data at a specified point, adjusts array to make room
+	//--------------------------------------------------------------------------------------------------|
+	//									INSERT IN MIDDLE												|
+	//	Function to insert the data at a point in the array specified by the user. Takes two parameters,|
+	//	one being the actual data you want inserted into the array, and the index for which you want    |
+	//	the data to be inserted.																		|
+	//--------------------------------------------------------------------------------------------------|
+	void InsertInMiddle(T newData, int index)				
 	{
-		if (m_nUsedElements == m_nArraySize)					//Checks to see if the array is already full
+		if (m_nUsedElements == m_nArraySize)				
 		{
 			ResizeArray();
 		}
@@ -119,20 +112,55 @@ public:
 		m_nUsedElements += 1;
 	}
 
+	//--------------------------------------------------------------------------------------------------|
+	//									REMOVE INDEX													|
+	//  Function to remove the piece of data at the specified index. Takes 2 values, one being the      |
+	//	index you wish to remove data at, the other being a bool to decide whether or not you wish to	|
+	//	keep the data in their current order															|
+	//--------------------------------------------------------------------------------------------------|
+	void RemoveIndex(int index, bool ordered)			
+	{													
+		if (ordered == true)								
+		{
+			for (int i = index; i < m_nUsedElements; i++)
+			{
+				data[i] = data[i + 1];
+			}
+		}
+		else if (ordered == false)
+		{
+			data[index] = data[m_nUsedElements - 1];
+		}
+		m_nUsedElements -= 1;
+	}
+
+	//--------------------------------------------------------------------------------------------------|
+	//									CLEAR ARRAY														|
+	//	Acts as a "reset" for the array, clearing everything inside it.									|
+	//--------------------------------------------------------------------------------------------------|
+	void ClearArray()
+	{
+		for (int i = 0; i < m_nUsedElements; i++)
+		{
+			data[i] = NULL;
+		}
+	}
+
 private:
 
 	T * data;
 	int m_nArraySize;
 	int m_nUsedElements;
 
-
-
-	//FUNCTIONS FOR ADDING 1 OR MORE ELEMENTS FROM THE MIDDLE OF THE ARRAY
-
-	//FUNCTIONS FOR REMOVING FROM THE MIDDLE OF THE ARRAY, WITH BOTH ORDERED AND UNORDERED REMOVAL
-
-	//A FUNCTION FOR CLEARING THE ARRAY
-		
-
+	//Challenging additions:
+	//A function to sort the array
+	//A function to search the array for a given element
+	//A function for concatenating two dynamic arrays together
+	//A function for randomly shuffling the array
+	//A function for rotating the array by a given amount
+	// - Rotating an array means to shift elements in a direction
+	//		Any element that would fall off the end wrap back around to the start
+	//		eg: rotating the array {1,2,3,4,5,6,7,8,9} by 2 would give {8,9,1,2,3,4,5,6,7}
+	//			and rotating the array by -4 would give {5,6,7,8,9,1,2,3,4}
 };
 
--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -6,15 +6,18 @@ int main()
 {
 	DynamicArray<int> Array(10);
 
-	for (int i = 0; i <= 15 ; i++)
+	for (int i = 0; i <= 10 ; i++)
 	{
 		Array.AddToArrayEnd(i);
 	}
 
+	Array.PrintArray();	
+
+	Array.RemoveIndex(8, true);
+
 	Array.PrintArray();
 
-	Array.InsertInMiddle(43, 6);
-	Array.InsertInMiddle(73, 2);
+	Array.ClearArray();
 
 	Array.PrintArray();
 
